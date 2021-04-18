b3ad6f198dd80983c260cd5c44ec9c741fb74ce4
u3557110 <47244396+u3557110@users.noreply.github.com>
2021-04-18 16:32:34 +0800

    insert takes data

--- insert_data.py
+++ insert_data.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from datetime import datetime, date
+import calendar
 
 myconn = mysql.connector.connect(host="localhost", user="root", passwd="00624ckn", database="facerecognition")
 date = datetime.utcnow()
@@ -15,9 +16,15 @@ today_date = date.today().strftime('%Y-%m-%d')
 # val = ("123", "Kin", now_time, today_date, "2018-09-01", "CE", "u3557110@connect.hku.hk")
 # cursor.execute(sql, val)
 
-sql = "UPDATE student SET email = 'u3557110@connect.hku.hk' WHERE student_id = '1'"
-cursor.execute(sql)
+# sql = "UPDATE student SET email = 'u3557110@connect.hku.hk' WHERE student_id = '1'"
+# cursor.execute(sql)
+#
+# sql = "INSERT INTO course (courseID, course_title, lecture_room_address, start_time, end_time, weekday, teacher_name, link, department) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s)"
+# val = ("comp3278", "Intro to DB", "off-site", "17:00:00", "20:00:00", "Sunday", "Ping Luo", "https://hku.zoom.us/j/97686555806?pwd=NWxSNVRKTlNDU0NjYTgremxaQ3pldz09", "Computer Science")
+# cursor.execute(sql, val)
 
-
+sql = "INSERT INTO takes (student_id, course_id) VALUES (%s, %s)"
+val = ("1", "comp3278")
+cursor.execute(sql, val)
 
 myconn.commit()
